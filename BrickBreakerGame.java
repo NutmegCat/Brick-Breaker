b03ac73bba12e2a2e5b3b3d60b6412b66561a0f9
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-11-16 17:14:06 -0500

    completed necessary methods

--- BrickBreakerGame.java
+++ BrickBreakerGame.java
@@ -2,6 +2,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
 import javax.swing.*;
+import java.awt.*;
 
 public class BrickBreakerGame extends JFrame implements ActionListener, KeyListener {
     private static final int WIDTH = 400;
@@ -44,13 +45,64 @@ public class BrickBreakerGame extends JFrame implements ActionListener, KeyListe
     }
 
     private void moveBall() {
+        ballX += ballSpeedX;
+        ballY += ballSpeedY;
 
+        if (ballX <= 0 || ballY >= WIDTH - 20) {
+            ballSpeedX = -ballSpeedX;
+        }
+
+        if (ballY <= 0) {
+            ballSpeedY = -ballSpeedY;
+        }
+
+        if (ballY >= HEIGHT - 20) {
+            // Game over
+            timer.stop();
+            JOptionPane.showMessageDialog(this, "Game Over!\nYour score: " + score);
+            System.exit(0);
+        }
     }
     private void movePaddle() {
-
+        if (paddleX < 0) {
+            paddleX = 0;
+        } else if (paddleX > WIDTH - 100) {
+            paddleX = WIDTH - 100;
+        }
     }
 
     private void checkCollision() {
+        Rectangle ballRect = new Rectangle(ballX, ballY, 20, 20);
+        Rectangle paddleRect = new Rectangle(paddleX, HEIGHT - 30, 100, 20);
 
+        if (ballRect.intersects(paddleRect)) {
+            ballSpeedY = -ballSpeedY;
+        }
+
+        for (int i = 0; i < bricks.length; i++) {
+            if (bricks[i]) {
+                Rectangle brickRect = new Rectangle((i % 10) * 40, (i / 10) * 20, 40, 20);
+                if (ballRect.intersects(brickRect)) {
+                    ballSpeedY = -ballSpeedY;
+                    bricks[i] = false;
+                    score += 10;
+
+                    // Check for victory
+                    boolean win = true;
+                    for (boolean brick : bricks) {
+                        if (brick) {
+                            win = false;
+                            break;
+                        }
+                    }
+
+                    if (win) {
+                        timer.stop();
+                        JOptionPane.showMessageDialog(this, "You Win!\nYour score: " + score);
+                        System.exit(0);
+                    }
+                }
+            }
+        }
     }
 }
