1f1f52cb003a70bc4cb113effa6bae093b1f29ba
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-11-16 15:47:46 -0500

    core element variables

--- BrickBreakerGame.java
+++ BrickBreakerGame.java
@@ -5,4 +5,8 @@ public class BrickBreakerGame extends JFrame {
     private static final int HEIGHT = 600;
 
     private Timer timer;
+    private int ballX, ballY, ballSpeedX, ballSpeedY;
+    private int paddleX, paddleY;
+    private boolean[] bricks;
+    private int score;
 }
