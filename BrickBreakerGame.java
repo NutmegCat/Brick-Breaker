dea50b59553fb8fc0b9e6da408c340f6af4fcbfb
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-11-16 15:46:14 -0500

    used imports for timer and JFrame

--- BrickBreakerGame.java
+++ BrickBreakerGame.java
@@ -1,6 +1,8 @@
-public class BrickBreakerGame {
+import javax.swing.*;
+
+public class BrickBreakerGame extends JFrame {
     private static final int WIDTH = 400;
     private static final int HEIGHT = 600;
 
-
+    private Timer timer;
 }
